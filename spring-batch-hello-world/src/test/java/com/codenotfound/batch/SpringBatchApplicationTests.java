0d9a848043c84f6f5cd4deb75b419e87bfe2411e
CodeNotFound <codenotfound.com@gmail.com>
2018-10-10 20:26:44 +0200

    update unit test case

--- spring-batch-hello-world/src/test/java/com/codenotfound/batch/SpringBatchApplicationTests.java
+++ spring-batch-hello-world/src/test/java/com/codenotfound/batch/SpringBatchApplicationTests.java
@@ -1,14 +1,46 @@
 package com.codenotfound.batch;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobExecution;
+import org.springframework.batch.core.launch.NoSuchJobException;
+import org.springframework.batch.test.JobLauncherTestUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.codenotfound.batch.job.HelloWorldJobConfig;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@SpringBootTest(classes = {SpringBatchApplicationTests.BatchTestConfig.class})
 public class SpringBatchApplicationTests {
 
+  @Autowired
+  private JobLauncherTestUtils jobLauncherTestUtils;
+
   @Test
-  public void contextLoads() {}
+  public void testHelloWorldJob() throws Exception {
+    JobExecution jobExecution = jobLauncherTestUtils.launchJob();
+    assertThat(jobExecution.getExitStatus().getExitCode()).isEqualTo("COMPLETED");
+  }
+
+  @Configuration
+  @Import(HelloWorldJobConfig.class)
+  static class BatchTestConfig {
+
+    @Autowired
+    private Job helloWorlJob;
+
+    @Bean
+    JobLauncherTestUtils jobLauncherTestUtils() throws NoSuchJobException {
+      JobLauncherTestUtils jobLauncherTestUtils = new JobLauncherTestUtils();
+      jobLauncherTestUtils.setJob(helloWorlJob);
+
+      return jobLauncherTestUtils;
+    }
+  }
 }
