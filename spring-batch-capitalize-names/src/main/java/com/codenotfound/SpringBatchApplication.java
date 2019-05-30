79b247850d2b1a6a2eec09c5e61707ee309e57e4
code-not-found <codenotfound.com@gmail.com>
2019-05-30 21:12:31 +0200

    update spring and java version + restructure

--- spring-batch-capitalize-names/src/main/java/com/codenotfound/SpringBatchApplication.java
+++ spring-batch-capitalize-names/src/main/java/com/codenotfound/SpringBatchApplication.java
@@ -1,10 +1,11 @@
-package com.codenotfound.batch;
+package com.codenotfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
+@SpringBootApplication(
+    exclude = {DataSourceAutoConfiguration.class})
 public class SpringBatchApplication {
 
   public static void main(String[] args) {
