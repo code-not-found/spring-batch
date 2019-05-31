ff9220e0af51def109ab3f4895cb04b6c37978e1
code-not-found <codenotfound.com@gmail.com>
2019-05-31 08:39:59 +0200

    update spring and java version + restructure

--- spring-batch-admin/spring-cloud-data-flow-server/src/main/java/com/codenotfound/SpringDataFlowServerApplication.java
+++ spring-batch-admin/spring-cloud-data-flow-server/src/main/java/com/codenotfound/SpringDataFlowServerApplication.java
@@ -1,14 +1,17 @@
-package com.codenotfound.batch;
+package com.codenotfound;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.dataflow.server.EnableDataFlowServer;
+import org.springframework.cloud.deployer.spi.cloudfoundry.CloudFoundryDeployerAutoConfiguration;
 
 @EnableDataFlowServer
-@SpringBootApplication
+@SpringBootApplication(
+    exclude = {CloudFoundryDeployerAutoConfiguration.class})
 public class SpringDataFlowServerApplication {
 
   public static void main(String[] args) {
-    SpringApplication.run(SpringDataFlowServerApplication.class, args);
+    SpringApplication.run(SpringDataFlowServerApplication.class,
+        args);
   }
 }
